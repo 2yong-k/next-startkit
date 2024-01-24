673ddf7539b353c85e29e3982d79f0f533e8e576
kdy <kkykdy4334@gmail.com>
2024-01-24 13:20:04 +0900

    feat: update starter kit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-// module.exports = async (phase, { defaultConfig }) => {
-    /**
-     * @type {import('next').NextConfig}
-     */
-    const nextConfig = {
-      reactStrictMode: true, // react <React.StrictMode>
-      swcMinify: true, // like Terser
-    };
-    return nextConfig;
-// };
+module.exports = {
+  reactStrictMode: true, // react <React.StrictMode>
+  swcMinify: true, // like Terser
+};
